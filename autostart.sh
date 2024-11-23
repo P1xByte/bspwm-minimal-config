363f3fc759fbe933447b592a00d84dc20234855e
P1xByte <109861687+P1xByte@users.noreply.github.com>
2024-11-23 12:23:22 +0300

    Update autostart.sh

--- autostart.sh
+++ autostart.sh
@@ -4,4 +4,4 @@ pgrep -x sxhkd > /dev/null || sxhkd &
 numlockx on &
 xsetroot -cursor_name left_ptr &
 setxkbmap -option grp:alt_shift_toggle us,ru &
-feh --bg-fill $HOME/Pictures/Wallpapers/penger_field_work.png
+
